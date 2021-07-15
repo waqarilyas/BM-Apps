957e28cd95141831f425eb9f45aec1fbf56d343e
Salman <salman@kryptomind.com>
2021-07-15 12:10:16 +0500

    Svg uri icon support

--- src/shared/utils/sockets.tsx
+++ src/shared/utils/sockets.tsx
@@ -7,7 +7,7 @@ export const initSocket = async (address: string) => {
   try {
     if (address) {
       console.log('\x1b[32m', 'Removing Listener');
-      socket.removeAllListeners();
+      socket.removeEventListener(address);
     } else {
       return;
     }
