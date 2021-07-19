c8d0b6fd60ab2749ef06503982c0b1bd20830e2f
Salman <salman@kryptomind.com>
2021-07-19 18:18:43 +0500

    Wallet Integration

--- block.config.tsx
+++ block.config.tsx
@@ -10,7 +10,7 @@ const blockConfig = {
   /**
     Configuration for Rinkeby
     */
-  API_URL: 'http://192.168.1.191:4000',
+  API_URL: 'https://api.blockmerchants.com',
   INFURA_URL: 'https://rinkeby.infura.io/v3/b83bc7f34f61479e89b7f43c3175f2c7',
   ETH_GAS_API: 'https://ethgasstation.info/json/ethgasAPI.json',
   CHAIN: 'rinkeby',
--- src/assets/coins/index.tsx
+++ src/assets/coins/index.tsx
@@ -1,38 +1,17 @@
 const COINS = {
-  BCH: require('./BCH.png'),
-  BTC: require('./BTC.png'),
-  CRD: require('./CRD.png'),
-  DASH: require('./DASH.png'),
-  DOGE: require('./DOGE.png'),
-  ETH: require('./ETH.png'),
-  LEVELG: require('./LEVELG.png'),
-  LTC: require('./LTC.png'),
-  USDT: require('./USDT.png'),
-  XLM: require('./XLM.png'),
+  BTC: require('./btc.png'),
+  ETH: require('./eth.png'),
+  WEENUS: require('./weenus.png'),
 };
 
 function GetImageForCoin(COIN: string) {
   COIN = COIN.toUpperCase();
-  if (COIN === 'BCH') {
-    return COINS.BCH;
-  } else if (COIN === 'BTC') {
+  if (COIN === 'BTC') {
     return COINS.BTC;
-  } else if (COIN === 'CRD') {
-    return COINS.CRD;
-  } else if (COIN === 'DASH') {
-    return COINS.DASH;
-  } else if (COIN === 'DOGE') {
-    return COINS.DOGE;
   } else if (COIN === 'ETH') {
     return COINS.ETH;
-  } else if (COIN === 'LEVELG') {
-    return COINS.LEVELG;
-  } else if (COIN === 'LTC') {
-    return COINS.LTC;
   } else if (COIN === 'WEENUS' || COIN === 'USDT') {
-    return COINS.USDT;
-  } else if (COIN === 'XLM' || COIN === 'native') {
-    return COINS.XLM;
+    return COINS.WEENUS;
   }
 }
 export {GetImageForCoin, COINS};
