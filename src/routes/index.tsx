efc22ba99318f5b899761e2ef16d6d8f5b0e7204
Salman <salman@kryptomind.com>
2021-08-02 18:19:10 +0500

    fixes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {THEME} from '../shared/theme';
@@ -8,36 +8,21 @@ import AuthStack from './Auth/Auth.routes';
 import BottomTabs from './Tabs/Tabs.routes';
 import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../shared/store';
-import axios from 'axios';
-import blockConfig from '../../block.config';
 import {renderWallet} from '../shared/store/actions/walletActions';
-import {AppShowToast} from '../shared/services/helper.service';
 
 const Routes = () => {
-  const {isRendered, wallet} = useSelector((state: RootState) => state.wallet);
+  const {isRendered} = useSelector((state: RootState) => state.wallet);
   const dispatch = useDispatch();
 
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
-  const [totalCoins, setTotalCoins] = useState(0);
-
-  useEffect(() => {
-    axios
-      .get(`${blockConfig.API_URL}/coin-rates/list/coins`)
-      .then(response => {
-        setTotalCoins(response.data.length);
-      })
-      .catch(err => console.log('Error getting coins list:', err));
-  }, []);
-
   useEffect(() => {
     if (isRendered) {
-      console.log('DIPATCHED');
       dispatch(renderWallet());
     }
-  }, [isRendered]);
+  }, [isRendered, dispatch]);
 
   return (
     <>
