597e01339289494ef720d02f12d4405685ecd14c
Muhammad Waqar <muhammad.waqar@kryptomind.com>
2021-11-08 15:08:39 +0500

    shifted to mainnet

--- block.config.tsx
+++ block.config.tsx
@@ -11,11 +11,12 @@ const blockConfig = {
     Configuration for Rinkeby
     */
   API_URL: 'https://api.blockmerchants.com',
-  INFURA_URL: 'https://rinkeby.infura.io/v3/b83bc7f34f61479e89b7f43c3175f2c7',
+  // API_URL: 'https://9031-125-209-76-78.ngrok.io',
+  INFURA_URL: 'https://mainnet.infura.io/v3/b83bc7f34f61479e89b7f43c3175f2c7',
   ETH_GAS_API: 'https://ethgasstation.info/json/ethgasAPI.json',
-  CHAIN: 'rinkeby',
-  CHAIN_ID: 4,
-  ENV: 'development',
+  CHAIN: 'mainnet',
+  CHAIN_ID: 1,
+  ENV: 'production',
 
   BSCSCAN_EXPLORER_URL: 'https://bscscan.com/',
   BSCSCAN_API_URL: 'https://api.bscscan.com',
@@ -30,6 +31,7 @@ const blockConfig = {
   ENC_SECRET: 'KM_KEY',
   ETHERSCAN_API_KEY: 'HZRT7R3NQVR5QN2QP3DDFA7KZ98J9FH1X6',
   BSCSCAN_API_KEY: '2FVGV9HMUSNR6QX4BS7IWY32H1UT416G2B',
+  BLOCKCYPHER_API_ENV: 'main',
 };
 
 const testnetConfig = {
@@ -63,6 +65,6 @@ const testnetConfig = {
   BSCSCAN_API_KEY: '2FVGV9HMUSNR6QX4BS7IWY32H1UT416G2B',
 };
 
-const defaultConfig = testnetConfig;
+const defaultConfig = blockConfig;
 
 export default defaultConfig;
